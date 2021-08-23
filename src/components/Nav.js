ed0c7166b284abac1ce07482a7ef2434d9a4a1da
KENKEN0803 <blackflamekr@gmail.com>
2021-08-24 06:27:49 +0900

    CRUD

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,8 +2,9 @@ import {Link} from "react-router-dom";
 function Nav() {
     return (
         <div className="App">
-            <Link to="/">리스트보기</Link> &emsp;
+            <h5><Link to="/">리스트보기</Link> &emsp;
             <Link to="/writer">새 글 쓰기</Link>
+            </h5>
         </div>
     )
 }
