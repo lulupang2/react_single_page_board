5af9e1045daf2c3240e23082e8cc93dc1629950c
KENKEN0803 <blackflamekr@gmail.com>
2021-08-23 10:33:58 +0900

    CRUD 구현

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function Footer() {
 function App() {
   const [page, setPage] = useState(0);
   return (
-      <HashRouter>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Header/>
         <Nav/>
         <Route path="/" exact={true} component={Table}/>
