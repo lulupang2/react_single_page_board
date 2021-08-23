5aa2e33f0e281e9fb5c1069fc883bc32e46b312e
KENKEN0803 <blackflamekr@gmail.com>
2021-08-23 21:10:43 +0900

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import {useEffect, useState} from "react";
 import {HashRouter, Route} from "react-router-dom";
 import {Link} from "react-router-dom";
 import Table from './components/Table';
@@ -8,38 +7,37 @@ import Modify from './components/Modify';
 
 
 function Header() {
-  return (
-      <h1 className="header">헤더영역</h1>
-  )
+    return (
+        <h1 className="header">헤더영역</h1>
+    )
 }
 
 function Nav() {
-  return (
-      <div className="App">
-        <Link to="/">리스트</Link> <Link to="/writer">글쓰기</Link>
-      </div>
-  )
+    return (
+        <div className="App">
+            <Link to="/">리스트</Link> <Link to="/writer">글쓰기</Link>
+        </div>
+    )
 }
 
 function Footer() {
-  return (
-      <h4 className="footer">풋터 영역</h4>
-  )
+    return (
+        <h4 className="footer">풋터 영역</h4>
+    )
 }
 
 function App() {
-  const [page, setPage] = useState(0);
-  return (
-      <HashRouter>
-        <Header/>
-        <Nav/>
-        <Route path="/" exact={true} component={Table}/>
-        <Route path="/writer" component={Writer}/>
-        <Route path="/modify" component={Modify}/>
-        <Footer/>
-      </HashRouter>
-  )
-      ;
+    return (
+        <HashRouter>
+            <Header/>
+            <Nav/>
+            <Route path="/" exact={true} component={Table}/>
+            <Route path="/writer" component={Writer}/>
+            <Route path="/modify" component={Modify}/>
+            <Footer/>
+        </HashRouter>
+    )
+        ;
 }
 
 export default App;
