ac07c07f4d956f3eab88d3c1e0ea160238c119a1
lulupang2 <lulupang@live.co.kr>
2021-08-28 20:15:37 +0900

    d

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,28 +26,23 @@ function NavBar() {
                             <Link to="/loc"><a className="nav-link">점심 뭐먹지</a></Link>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                               data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr className="dropdown-divider"/>
-                                </li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                            <Dropdown>
+                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                    Dropdown Button
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
                         </li>
 
-                        <li className="nav-item">
-                            <Dropdown>
-
-                            </Dropdown>
-                        </li>
+                    
                     </ul>
                     <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
